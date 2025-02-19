0a5ff626db79bb2898cf6e31223ed126675b0fdd
(＃￣ω￣) <mr.mmorz@list.ru>
2025-02-20 02:34:55 +0400

    feat: Добавление документации + логирования

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const routes = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swaggerConfig');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(morgan('[:method] :url'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api', routes)
 
