c0155ba423f5650aa3efb0af3f0ab211e72e7c95
(＃￣ω￣) <mr.mmorz@list.ru>
2025-03-06 03:14:04 +0400

    Проверка husky и lint-staged

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,12 @@ import { authenticate, sequelize } from './config/db';
 
 const start: () => Promise<void> = async (): Promise<void> => {
     try {
-            await authenticate();
+                await authenticate();
 
         a   wait sequelize.sync({ force: false });
 
-                    app.listen(config.server.port, (): void => console.log(`Server run on ${config.server.port}`));
-    } catch (e: unknown) {
+                            app.listen(config.server.port, (): void => console.log(`Server run on ${config.server.port}`));
+    }       catch (e: unknown) {
         console.error('Error starting the server: ', e);
     }
 };
