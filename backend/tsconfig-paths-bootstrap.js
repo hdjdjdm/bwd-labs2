d39391bc278d83976845cdd5b6e9103cf3d87024
(＃￣ω￣) <mr.mmorz@list.ru>
2025-03-12 10:20:53 +0400

    Init frontend

--- backend/tsconfig-paths-bootstrap.js
+++
@@ -1,7 +0,0 @@
-import { register } from 'tsconfig-paths';
-import tsConfig from './tsconfig.json';
-
-register({
-    baseUrl: tsConfig.compilerOptions.outDir,
-    paths: tsConfig.compilerOptions.paths,
-});
