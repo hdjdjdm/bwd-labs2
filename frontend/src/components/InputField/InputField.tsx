71f5d27dc508540ee80f1cf74191caf6c4364567
(＃￣ω￣) <mr.mmorz@list.ru>
2025-04-17 13:32:00 +0400

    fix: ошибки при валидации inputfield теперь показываются в customToast, а не в обычном предупреждении от inputField

--- frontend/src/components/InputField/InputField.tsx
+++ frontend/src/components/InputField/InputField.tsx
@@ -1,6 +1,7 @@
 import styles from './InputField.module.scss';
-import React from 'react';
+import React, { useRef } from 'react';
 import classNames from 'classnames';
+import { showCustomToast } from '@utils/customToastUtils.ts';
 
 interface InputFieldProps {
     type: string;
@@ -29,14 +30,56 @@ const InputField: React.FC<InputFieldProps> = ({
     required = false,
     disabled = false,
 }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
     const inputValue =
         type === 'date' && value instanceof Date
             ? value.toISOString().split('T')[0]
             : value;
 
+    const handleInvalid = (e: React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const validity = e.currentTarget.validity;
+        if (validity.valid) {
+            inputRef.current?.classList.remove(
+                styles.inputField__input_warning,
+            );
+            return;
+        }
+
+        inputRef.current?.classList.add(styles.inputField__input_warning);
+        if (validity.valueMissing) {
+            showCustomToast(`Field "${label}" is required.`, 'warning');
+        } else if (validity.typeMismatch && type === 'email') {
+            showCustomToast('Incorrect email.', 'warning');
+        } else if (validity.tooLong) {
+            showCustomToast(
+                `Max length — ${maxLength} characters long.`,
+                'warning',
+            );
+        } else if (validity.tooShort) {
+            showCustomToast(
+                `Min length — ${e.currentTarget.minLength} characters long.`,
+                'warning',
+            );
+        } else {
+            showCustomToast(`Incorrect input for field "${label}".`, 'warning');
+        }
+    };
+
+    const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+        if (e.currentTarget.validity.valid) {
+            inputRef.current?.classList.remove(
+                styles.inputField__input_warning,
+            );
+        }
+        onChange(e as React.ChangeEvent<HTMLInputElement>);
+    };
+
     return (
         <div className={styles.inputField}>
             <input
+                ref={inputRef}
                 className={classNames(
                     styles.inputField__input,
                     iconSrc && styles.inputField__input_withIcon,
@@ -44,12 +87,13 @@ const InputField: React.FC<InputFieldProps> = ({
                 )}
                 type={type}
                 value={inputValue as string}
-                onChange={onChange}
+                onChange={handleInput}
                 placeholder={placeholder}
                 minLength={type === 'password' ? 6 : 3}
                 maxLength={maxLength}
                 required={required}
                 disabled={disabled}
+                onInvalid={handleInvalid}
             />
             <label className={styles.inputField__label}>{label}</label>
             <img
